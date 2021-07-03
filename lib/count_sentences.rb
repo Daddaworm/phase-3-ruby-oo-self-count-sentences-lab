a6178a1851de1dc817ccdded96bac35ae863ed70
Ethan Rodriguez <ethan.rodriguez@mac.com>
2021-07-02 21:57:01 -0400

    commit tree 4c03b4199754e41a0e062752ac51c3551de96e04

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,23 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("Hi, my name is Sophie!")
   end
 
   def count_sentences
-
+    self.split(/\.|\?|\!/).delete_if do |w|
+       w.size < 2
+    end.size
+    # binding.pry
   end
 end
+
+# "This, well, is a sentence. This is too!! And so is this, I think? Woo..."
